b4a52a27239474d3404f2405249b99f3b12e1b79
QUANTORIS01 <quantoris01@gmail.com>
2026-07-02 19:45:45 +0330

    Implementation of a custom user model along with a set of configurations.

--- config/accounts/admin.py
+++ config/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ['username', 'phone', 'role', 'first_name', 'last_name']
+    list_filter = ['role']
+    search_fields = ['username', 'phone', 'email']
+    list_editable = ['role']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information',
+         {'fields': ('role', 'phone', 'date_of_birth', 'bio', 'job','avatar')}),)
--- config/accounts/models.py
+++ config/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        USER = 'user', 'User'
+        AUTHOR = 'author', 'Author'
+        ADMIN = 'admin', 'Admin'
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER, verbose_name='User Role')
+    bio = models.TextField(blank=True, null=True, verbose_name='Biography')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Avatar')
+    job = models.CharField(max_length=100, blank=True, null=True, verbose_name='job')
+    phone = models.CharField(max_length=11, blank=True, null=True, verbose_name='Phone Number')
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name='Date of birth')
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
+
+
