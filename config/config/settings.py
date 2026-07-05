13c7bbf901d77fd9524237ed98433e42d377589c
QUANTORIS01 <quantoris01@gmail.com>
2026-07-05 21:14:12 +0330

    refactor: rename images directory to media

--- config/config/settings.py
+++ config/config/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.User'
 
